87f08b7216b382d79ee634753a003c1f095dd211
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-03 12:48:41 +0300

    cfhj

--- BeautySalon.Booking.Persistence/Context/BookingDbContextFactory.cs
+++ BeautySalon.Booking.Persistence/Context/BookingDbContextFactory.cs
@@ -5,10 +5,10 @@ namespace BeautySalon.Booking.Persistence.Context;
 
 public class BookingDbContextFactory : IDesignTimeDbContextFactory<BookingDbContext>
 {
-    public BookingDbContext CreateDbContext(string[] args)
+    public BookingDbContext CreateDbContext(string[] argsZ)
     {
         var optionsBuilder = new DbContextOptionsBuilder<BookingDbContext>();
-        optionsBuilder.UseNpgsql("Host=booking-service-api-postgres;Port=5432;Database=BeautySalonBookingDb;Username=postgres;Password=1234"); // Подставь строку подключения
+        optionsBuilder.UseNpgsql("Host=booking-service-api-postgres;Port=5432;Database=BeautySalonBookingDb;Username=postgres;Password=1234");
 
         return new BookingDbContext(optionsBuilder.Options);
     }
--- BeautySalon.Booking.Persistence/DependencyInjection.cs
+++ BeautySalon.Booking.Persistence/DependencyInjection.cs
@@ -11,7 +11,7 @@ namespace BeautySalon.Booking.Persistence
     {
         public static IServiceCollection AddPersistance(this IServiceCollection service, IConfiguration confing)
         {
-            var connectionString = confing.GetConnectionString("BookingDatabase");
+            var connectionString = confing.GetConnectionString("Host=booking-service-api-postgres;Port=5432;Database=BeautySalonBookingDb;Username=postgres;Password=1234");
             
             service.AddDbContext<BookingDbContext>(o =>
             {
