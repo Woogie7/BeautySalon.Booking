8108fe4dadffb9615a04cca636a2e679ce9325f3
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-03 12:18:46 +0300

    тест

--- BeautySalon.Booking.Persistence/Context/BookingDbContextFactory.cs
+++ BeautySalon.Booking.Persistence/Context/BookingDbContextFactory.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace BeautySalon.Booking.Persistence.Context;
 
-public class BookingDbContextFactory : IDesignTimeDbContextFactory<BookingDbContext>
-{
-    public BookingDbContext CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<BookingDbContext>();
-        optionsBuilder.UseNpgsql("Host=booking-service-api-postgres;Port=5432;Database=BeautySalonBookingDb;Username=postgres;Password=1234"); // Подставь строку подключения
-
-        return new BookingDbContext(optionsBuilder.Options);
-    }
-}
+// public class BookingDbContextFactory : IDesignTimeDbContextFactory<BookingDbContext>
+// {
+//     public BookingDbContext CreateDbContext(string[] args)
+//     {
+//         var optionsBuilder = new DbContextOptionsBuilder<BookingDbContext>();
+//         optionsBuilder.UseNpgsql("Host=booking-service-api-postgres;Port=5432;Database=BeautySalonBookingDb;Username=postgres;Password=1234"); // Подставь строку подключения
+//
+//         return new BookingDbContext(optionsBuilder.Options);
+//     }
+// }
