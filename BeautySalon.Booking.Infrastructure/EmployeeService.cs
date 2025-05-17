894264a6e03fcfa29b630bd5c2524875760126d6
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-17 19:47:41 +0300

    eeeee

--- BeautySalon.Booking.Infrastructure/EmployeeService.cs
+++ BeautySalon.Booking.Infrastructure/EmployeeService.cs
@@ -5,6 +5,7 @@ using BeautySalon.Booking.Application.Models;
 using BeautySalon.Booking.Domain.AggregatesModel.BookingAggregate.ValueObjects;
 using BeautySalon.Booking.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BeautySalon.Booking.Application.Service
 {
@@ -13,39 +14,39 @@ namespace BeautySalon.Booking.Application.Service
         private readonly BookingDbContext _context;
         private readonly ICacheService _cacheService;
         private readonly IMapper _mapper;
+        private readonly ILogger<EmployeeReedService> _logger;
 
-        public EmployeeReedService(BookingDbContext context, ICacheService cacheService, IMapper mapper)
+        public EmployeeReedService(BookingDbContext context, ICacheService cacheService, IMapper mapper, ILogger<EmployeeReedService> logger)
         {
             _context = context;
             _cacheService = cacheService;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public async Task<bool> IsEmployeeExistsAsync(Guid employeeId)
-            {   
-                var cacheEmployee = await _cacheService.GetAsync<EmployeeDTO>($"employee{employeeId}");
-                if (cacheEmployee != null)
-                    return true;
-
-                var databaseEmployee = await GetByIdEmployeeAsync(employeeId);
-                if (databaseEmployee != null)
-                {
-                    var employeeDto = _mapper.Map<EmployeeDTO>(databaseEmployee);
-                    await _cacheService.SetAsync($"employee{employeeId}", employeeDto, TimeSpan.FromMinutes(5));
-                    return true;
-                }
-                
-
-                return false;
-            }
-        
-        public async Task<EmployeeReadModel?> GetByIdEmployeeAsync(Guid employeeId)
         {
-        
+            var cacheKey = $"employee{employeeId}";
+
+            var cached = await _cacheService.GetAsync<EmployeeDTO>(cacheKey);
+            if (cached != null)
+                return true;
+
             var employee = await _context.Employees
                 .AsNoTracking()
                 .FirstOrDefaultAsync(e => e.Id == employeeId);
-            return employee;
+            
+            _logger.LogInformation(employee.Name);
+
+            if (employee != null)
+            {
+                var dto = _mapper.Map<EmployeeDTO>(employee);
+                await _cacheService.SetAsync(cacheKey, dto, TimeSpan.FromMinutes(5));
+                return true;
+            }
+            
+            await _cacheService.SetAsync(cacheKey + ":notfound", "1", TimeSpan.FromMinutes(1));
+            return false;
         }
     }
 }
