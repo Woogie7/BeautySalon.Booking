bcbb71618cdbe5490beafaa4fc705dd92eb43840
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-22 00:19:20 +0300

    ещё

---
+++ BeautySalon.Booking.Persistence/Migrations/20250521211530_AddCustomDateOfWeeksSeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BeautySalon.Booking.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomDateOfWeeksSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookStatus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookStatus", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookStatus",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Processing" },
+                    { 2, "Confirmed" },
+                    { 3, "Canceled" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookStatus");
+        }
+    }
+}
