e636800dcf77be52404f3d62dea0af1e1aa5921d
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-06-19 21:33:25 +0300

    asd

--- BeautySalon.Booking.Infrastructure/Rabbitmq/EventBus.cs
+++ BeautySalon.Booking.Infrastructure/Rabbitmq/EventBus.cs
@@ -18,11 +18,10 @@ namespace BeautySalon.Booking.Infrastructure.Rabbitmq
         private readonly ResiliencePipeline _pipeline; 
         //private readonly IPendingQueueRepository _pendingQueueRepository;
 
-        public EventBus(IPublishEndpoint endpoint, ILogger<EventBus> logger, IPendingQueueRepository pendingQueueRepository)
+        public EventBus(IPublishEndpoint endpoint, ILogger<EventBus> logger)
         {
             _endpoint = endpoint;
             _logger = logger;
-            //_pendingQueueRepository = pendingQueueRepository;
 
             _pipeline = new ResiliencePipelineBuilder()
             .AddRetry(new RetryStrategyOptions
